c43abc15554e85edd566effea7e15851e14530fb
MeiSaM25970 <meisam25970@gmail.com>
2020-10-06 11:13:46 +0330

    create

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 
+const data = require("../material-dashboard-pro-html-v2.1.2/examples/pages/rtl.html");
 export class HomePage extends Component {
   render() {
-    return <h1>Home Page</h1>;
+    return <div className="abs" dangerouslySetInnerHTML={{ __html: data }} />;
   }
 }
